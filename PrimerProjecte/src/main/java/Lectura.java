11b58567ddd02c7dec58dac78f337065a23f0c34
Santi Sabate <ssabate@iesebre.com>
2022-09-19 14:20:55 +0200

    Primer commit

---
+++ PrimerProjecte/src/main/java/Lectura.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Lectura {
+
+    public static void main(String[] args) {
+        //1. Declaro una variable de tipo Scanner
+        Scanner ent = new Scanner(System.in);
+        int numero;
+        double real, suma;
+
+        //2. Vaig a llegir una dada... entera (int)
+        // i la guardo a una variable entera
+        System.out.println("Introduix un número enter i un número real:");
+        numero = ent.nextInt();
+        real = ent.nextDouble();
+
+        //3. Treballo en la variable de la manera que vulgue
+        System.out.println("La variable val "+numero);
+        System.out.println("la suma dels valors és "+numero+real);  //incorrecte, concatena enlloc de sumar!!
+        System.out.println("la suma dels valors és "+(numero+real)); //correcte, mostra la suma
+
+        suma = numero + real;                                       //correcte, però més llarg
+        System.out.println("la suma dels valors és " + suma);
+    }
+}
---
+++ PrimerProjecte/src/main/java/Variable.java
@@ -0,0 +1,33 @@
+public class Variable {  // nom classes --> PascalCase
+
+    public static void main(String[] args) {
+
+        //Zona de declaració de variables
+        int numero; //declaració de variable
+        numero = 25;//inicialització de la variable
+
+        char lletra;
+
+        int any = 2023;  //--> variable de tipo int i nom any
+        byte mes = 9;   //declaració + inicialització de variable
+        short diaMes = 12;    // nom de variables --> camelCase
+        double _1real = 2.3;
+        double real=3.4;
+
+        double real1, real2 = 3.4;    //Declaro 2 variables del mateix tipo
+        boolean int1=true;
+
+        //Codi del programa
+        System.out.println("Estem a l'any "+any);
+        System.out.println("Estem al mes "+mes+1);
+        System.out.println("Estem al dia "+(diaMes+1));
+
+        System.out.println("Estem a l'any "+any+" i al mes "+mes+" i al dia "+(diaMes+1));
+
+        //Inicialitzo la lletra
+        lletra = 'a';
+
+        //System.out.println(real1);
+
+    }
+}
